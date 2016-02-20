5a486e7a210904f2e23e3e998b72db6da86ac462
Alvin Lin <alvin.lin.dev@gmail.com>
2016-02-20 13:41:01 -0500

    shared utility file

---
+++ shared/Constants.js
@@ -0,0 +1,16 @@
+/**
+ * This class stores global constants between the client and server.
+ * @author Alvin Lin (alvin.lin.dev@gmail.com)
+ */
+
+/**
+ * Empty constructor for the Constants class.
+ * @constructor
+ */
+function Constants() {
+  throw new Error('Constants should not be instantiated!');
+}
+
+try {
+  module.exports = Constants;
+} catch (err) {}
